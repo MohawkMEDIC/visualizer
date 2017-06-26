56310cc13f76f89fa44408c9b6a76c8c9e4bdf09
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2017-06-26 00:59:06 -0400

    license headers

--- MARC.EHRS.Visualization.Server.Syslog/TransportProtocol/TcpTransport.cs
+++ MARC.EHRS.Visualization.Server.Syslog/TransportProtocol/TcpTransport.cs
@@ -1,5 +1,5 @@
-﻿/**
- * Copyright 2012-2013 Mohawk College of Applied Arts and Technology
+﻿/*
+ * Copyright 2012-2017 Mohawk College of Applied Arts and Technology
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you 
  * may not use this file except in compliance with the License. You may 
@@ -14,9 +14,8 @@
  * the License.
  * 
  * User: fyfej
- * Date: 13-8-2012
+ * Date: 2012-6-15
  */
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
